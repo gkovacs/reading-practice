16dc5f041adddc25416a9f9e1f1a5c0dae35a961
Geza Kovacs <gkovacs@users.noreply.github.com>
2011-04-06 02:01:08 +0000

    added a contribute sentences tab

---
+++ ReadingPractice/ReadingPractice/ContributeSentenceTab.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ReadingPractice
+{
+    public partial class ContributeSentenceTab : UserControl
+    {
+        public ContributeSentenceTab()
+        {
+            InitializeComponent();
+        }
+
+        private void ContributeButton_Click(object sender, RoutedEventArgs e)
+        {
+            string nativeSentence = this.NativeSentenceTextBox.Text;
+            string translatedSentence = this.TranslatedSentenceTextBox.Text;
+            this.ContributedSentenceListViewer.Children.Insert(1, new SentenceView(nativeSentence, translatedSentence));
+        }
+    }
+}
