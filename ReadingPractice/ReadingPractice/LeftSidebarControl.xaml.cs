d96aa61ae2a9b35a25092e5c14242e8f6c5988a9
Geza Kovacs <gkovacs@users.noreply.github.com>
2011-04-06 22:21:51 +0000

    added interface for left sidebar

--- ReadingPractice/ReadingPractice/LeftSidebarControl.xaml.cs
+++ ReadingPractice/ReadingPractice/LeftSidebarControl.xaml.cs
@@ -20,5 +20,32 @@ namespace ReadingPractice
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// TODO
+        /// getter returns empty string if no study focus is set (reviewing mode), returns study focus in
+        /// the foreign language being studied otherwise.
+        /// setter sets the study focus and updates GUI appropriately
+        /// </summary>
+        public string StudyFocus
+        {
+            get
+            {
+                return "";
+            }
+            set
+            {
+                value = "";
+            }
+        }
+
+        /// <summary>
+        /// TODO
+        /// returns true if this word (given in the foreign language) is allowed to be displayed in sentences
+        /// </summary>
+        public bool isDisplayed(string foreignWord)
+        {
+            return true;
+        }
     }
 }
