41616201ffdd65bb74311a8cd85884d5be0dbbbc
Geza Kovacs <gkovacs@users.noreply.github.com>
2011-04-06 05:08:48 +0000

    added word dictionary interface

---
+++ ReadingPractice/ReadingPractice/WordDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace ReadingPractice
+{
+    public static class WordDictionary
+    {
+        public static string translateToEnglish(string foreignWord, Languages language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string translateToForeign(string englishWord, Languages language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Dictionary<string, string> getEnglishToForeignDictionaryForLanguage(Languages language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Dictionary<string, string> getForeignToEnglishDictionaryForLanguage(Languages language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
