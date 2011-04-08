97740a715b0e405708eaf0921a9113792d529e4b
Geza Kovacs <gkovacs@users.noreply.github.com>
2011-04-08 20:07:29 +0000

    code cleanup

--- ReadingPractice/ReadingPractice/SentenceDictionarySimplifiedMandarin.cs
+++ ReadingPractice/ReadingPractice/SentenceDictionarySimplifiedMandarin.cs
@@ -16,10 +16,10 @@ namespace ReadingPractice
 {
     public class SentenceDictionarySimplifiedMandarin : SentenceDictionary
     {
-        Dictionary<Languages, List<string>> sentences = new Dictionary<Languages, List<string>>();
-        Dictionary<Languages, Dictionary<string, string[]>> segmentation = new Dictionary<Languages, Dictionary<string, string[]>>();
-        Dictionary<Languages, Dictionary<string, string>> foreignToEnglish = new Dictionary<Languages, Dictionary<string, string>>();
-        Dictionary<Languages, Dictionary<string, string>> englishToForeign = new Dictionary<Languages, Dictionary<string, string>>();
+        List<string> sentences = new List<string>();
+        Dictionary<string, string[]> segmentation = new Dictionary<string, string[]>();
+        Dictionary<string, string> foreignToEnglish = new Dictionary<string, string>();
+        Dictionary<string, string> englishToForeign = new Dictionary<string, string>();
         readonly WordDictionary _wordDictionary;
         readonly WordDictionary _traditionalWordDictionary;
         public WordDictionary wordDictionary
@@ -41,13 +41,6 @@ namespace ReadingPractice
         {
             _wordDictionary = new WordDictionarySimplifiedMandarin();
             _traditionalWordDictionary = new WordDictionaryTraditionalMandarin();
-            foreach (Languages lang in EnumHelper.GetValues<Languages>())
-            {
-                segmentation[lang] = new Dictionary<string, string[]>();
-                englishToForeign[lang] = new Dictionary<string, string>();
-                foreignToEnglish[lang] = new Dictionary<string, string>();
-                sentences[lang] = new List<string>();
-            }
             using (StreamReader reader = new StreamReader(Application.GetResourceStream(new Uri("ReadingPractice;component/cmn-simp-eng.txt", UriKind.Relative)).Stream))
             {
                 while (!reader.EndOfStream)
@@ -56,8 +49,8 @@ namespace ReadingPractice
                     string[] parts = line.Split('\t');
                     if (parts.Length != 2)
                         throw new Exception();
-                    foreignToEnglish[Languages.SimplifiedMandarin][parts[0]] = parts[1];
-                    englishToForeign[Languages.SimplifiedMandarin][parts[1]] = parts[0];
+                    foreignToEnglish[parts[0]] = parts[1];
+                    englishToForeign[parts[1]] = parts[0];
                 }
                 reader.Close();
             }
@@ -69,21 +62,21 @@ namespace ReadingPractice
                     string[] parts = line.Split('\t');
                     if (parts.Length != 2)
                         throw new Exception();
-                    segmentation[Languages.SimplifiedMandarin][parts[0]] = parts[1].Split(' ');
+                    segmentation[parts[0]] = parts[1].Split(' ');
                 }
                 reader.Close();
             }
-            foreach (string sent in foreignToEnglish[Languages.SimplifiedMandarin].Keys)
+            foreach (string sent in foreignToEnglish.Keys)
             {
-                sentences[Languages.SimplifiedMandarin].Add(sent);
+                sentences.Add(sent);
             }
         }
 
         public IList<string> getSentences(string focusWord, Func<string, bool> isWordAllowedFunc)
         {
-            return sentences[language].Where((sent) =>
+            return sentences.Where((sent) =>
             {
-                string[] words = segmentation[language][sent];
+                string[] words = segmentation[sent];
                 if (!words.Contains(focusWord))
                     return false;
                 foreach (string word in words)
@@ -97,16 +90,16 @@ namespace ReadingPractice
 
         public string translateToEnglish(string foreignSentence)
         {
-            if (!foreignToEnglish[language].ContainsKey(foreignSentence))
+            if (!foreignToEnglish.ContainsKey(foreignSentence))
                 return "";
-            return foreignToEnglish[language][foreignSentence];
+            return foreignToEnglish[foreignSentence];
         }
 
         public string translateToForeign(string englishSentence)
         {
-            if (!englishToForeign[language].ContainsKey(englishSentence))
+            if (!englishToForeign.ContainsKey(englishSentence))
                 return "";
-            return englishToForeign[language][englishSentence];
+            return englishToForeign[englishSentence];
         }
 
         public string[] getWords(string foreignSentence)
